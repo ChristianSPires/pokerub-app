3e44722d5e1575e9293fa7339c902496e901e6ea
Christian de Souza Pires <christianyourdan1@gmail.com>
2024-09-22 18:07:25 -0300

    feat: new service functions

--- pokerub_app/src/services/pokemonService.ts
+++ pokerub_app/src/services/pokemonService.ts
@@ -13,22 +13,70 @@ async function getPokemon(id: number | string) {
 
     return { ...pokemonData, category };
   } catch (error) {
-    console.error("Error fetching Pokémon:", error);
     return null;
   }
 }
 
-async function getPokemonList() {
-  const endpoint = `https://pokeapi.co/api/v2/pokemon?limit=20&offset=0`;
+async function getPokemonList(paginationEndpoint: string | undefined) {
+  let endpoint = paginationEndpoint
+    ? paginationEndpoint
+    : "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
 
   try {
     const response = await fetch(endpoint);
     const json = await response.json();
-    return json.results;
+    return json;
   } catch (error) {
     console.error("Error fetching Pokémon list:", error);
     return [];
   }
 }
 
-export { getPokemon, getPokemonList };
+async function getPokemonEvolutionChain(url: string) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch evolution chain: ${response.statusText}`
+      );
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching Pokémon Evolution Chain:", error);
+    return null;
+  }
+}
+
+async function getPokemonImage(pokemonName: string) {
+  try {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+    const data = await response.json();
+    return data.sprites.front_default;
+  } catch (error) {
+    console.error("Error fetching Pokémon Image:", error);
+    return null;
+  }
+}
+
+async function getPokemonSpecies(id: number | string) {
+  const speciesEndpoint = `https://pokeapi.co/api/v2/pokemon-species/${id}/`;
+
+  try {
+    const speciesResponse = await fetch(speciesEndpoint);
+    const speciesData = await speciesResponse.json();
+    return speciesData;
+  } catch (error) {
+    console.error("Error fetching Pokémon Species:", error);
+    return null;
+  }
+}
+
+export {
+  getPokemon,
+  getPokemonList,
+  getPokemonEvolutionChain,
+  getPokemonImage,
+  getPokemonSpecies,
+};
